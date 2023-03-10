258505884f4ebf140833ac85402d45d02e11acfa
martinskauan <kauanmartinsdapoca@gmail.com>
2023-03-10 15:08:00 -0300

    primeiras oficinas

---
+++ oficina03.js
@@ -0,0 +1,22 @@
+let temDinheiro = true;
+let temSol = falso;
+
+
+    if (temDinheiro){
+ if (temSol){
+
+     console.log ("Guaruja");
+ } else {
+    console.log("Restalrante");
+ 
+} 
+
+        } else {
+            
+    if (temSol){
+        console.log ("Ibirapuera");
+    }else {
+        console.log ("Aula");
+    }
+}
+ 
